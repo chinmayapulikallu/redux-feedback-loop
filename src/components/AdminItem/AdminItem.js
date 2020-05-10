108eeebde2db13d53db9ca31d57985fca0170fcb
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-05-10 15:03:41 -0500

    added delete icon to admin page

--- src/components/AdminItem/AdminItem.js
+++ src/components/AdminItem/AdminItem.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
 class AdminItem extends Component {
@@ -20,7 +21,8 @@ class AdminItem extends Component {
                         <TableCell align="left">{feedback.understanding}</TableCell>
                         <TableCell align="left">{feedback.support}</TableCell>
                         <TableCell align="left">{feedback.comments}</TableCell>
-                <TableCell align="left"><Button color="secondary" variant="contained" onClick={this.handleClick}>Delete</Button></TableCell>
+                        <TableCell align="left"><IconButton aria-label="delete" onClick={this.handleClick}>
+                            <DeleteIcon /></IconButton></TableCell>   
                     </TableRow>
                 
         )
